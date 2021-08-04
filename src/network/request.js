35d472f3ae4e009ff904cbd93aae2e4c5a9d851d
wy <tzyzst@163.com>
2021-08-04 20:53:00 +0800

    2021-8-4

---
+++ src/network/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export function request(config) {
+    // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:7878/api/m5',
+    timeout: 5000
+  })
+
+  // 2.axios的拦截器
+  instance.interceptors.request.use(config => {
+    // console.log(config);
+    // 请求拦截的作用：
+    // 比如config中的一些信息不符合服务器的要求 
+    // 比如每次发送网络请求时，都希望在界面看到请求图标
+    // 比如登录（token），必须携带一些信息
+    return config
+  }, err => {
+    console.log(err);
+  });
+  instance.interceptors.response.use(res => {
+    // console.log(res);
+    return res.data
+  }, err => {
+    console.log(err);
+  });
+
+  // 3.发送真正的网络请求
+  return instance(config)
+}
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,12 +7,27 @@
 </template>
 
 <script>
-
 import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultidata} from 'network/home';
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
   components: {
     NavBar
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
   }
 }
 </script>
